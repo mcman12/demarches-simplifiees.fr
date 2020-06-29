740300c8ab816a96a1aff5ecd77b054dc4bd879f
clemkeirua <clement@keiruaprod.fr>
2020-06-29 17:57:14 +0200

    added invalid token explanation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -13,9 +13,17 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      return render(file: 'users/confirmations/invalid_token')
+    end
+  end
 
   # protected
 
