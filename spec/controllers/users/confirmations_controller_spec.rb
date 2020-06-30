1bd81aaace9a4bc41acd00b7a3eaa9191d4f21bb
clemkeirua <clement@keiruaprod.fr>
2020-06-30 10:51:21 +0200

    add tests for the token validation screens

--- spec/controllers/users/confirmations_controller_spec.rb
+++ spec/controllers/users/confirmations_controller_spec.rb
@@ -1,5 +1,4 @@
 describe Users::ConfirmationsController, type: :controller do
-  let!(:user) { create(:user, :unconfirmed) }
   let(:confirmation_token) { user.confirmation_token }
 
   before do
@@ -8,6 +7,8 @@ describe Users::ConfirmationsController, type: :controller do
 
   describe '#show' do
     context 'when confirming within the auto-sign-in delay' do
+      let!(:user) { create(:user, :unconfirmed) }
+
       before do
         Timecop.travel(1.hour.from_now) {
           get :show, params: { confirmation_token: confirmation_token }
@@ -31,6 +32,7 @@ describe Users::ConfirmationsController, type: :controller do
     end
 
     context 'when the auto-sign-in delay has expired' do
+      let!(:user) { create(:user, :unconfirmed) }
       before do
         Timecop.travel(3.hours.from_now) {
           get :show, params: { confirmation_token: confirmation_token }
@@ -49,6 +51,52 @@ describe Users::ConfirmationsController, type: :controller do
 
       it 'redirects the user to the sign-in path' do
         expect(subject).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'with a user already confirmed' do
+      let!(:user) { create(:user, confirmation_token: 'some_token') }
+      let(:confirmation_token) { user.confirmation_token }
+
+      before do
+        get :show, params: { confirmation_token: confirmation_token }
+      end
+      render_views
+
+      it 'doesn’t sign in the user' do
+        expect(subject.current_user).to be(nil)
+        expect(subject.current_instructeur).to be(nil)
+        expect(subject.current_administrateur).to be(nil)
+      end
+
+      it 'informs the user and asks it user to sign in' do
+        expect(response.body).to have_content('Votre compte est déja activé')
+        expect(response.body).to have_content('Vous pouvez vous dès à présent vous connecter')
+      end
+    end
+
+    context 'with an invalid confirmation token' do
+      let!(:user) { create(:user, :unconfirmed) }
+      let(:confirmation_token) { 'an invalid confirmation token' }
+      before do
+        Timecop.travel(1.hour.from_now) {
+          get :show, params: { confirmation_token: confirmation_token }
+        }
+      end
+      render_views
+
+      it 'does not confirm the user' do
+        expect(user.reload).not_to be_confirmed
+      end
+
+      it 'doesn’t sign in the user' do
+        expect(subject.current_user).to be(nil)
+        expect(subject.current_instructeur).to be(nil)
+        expect(subject.current_administrateur).to be(nil)
+      end
+
+      it 'suggests the user to ask for a new confirmation token' do
+        expect(response.body).to have_content('Votre jeton de connection est invalide')
       end
     end
   end
